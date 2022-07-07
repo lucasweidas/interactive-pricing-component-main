281f35dcd41b7a5ca413639e78d7b189b550f8a8
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-07-07 15:21:00 -0300

    medium version styles and js code finished

---
+++ dist/js/main.js
@@ -0,0 +1,58 @@
+const slider = document.querySelector('[data-slider]');
+const toggle = document.querySelector('[data-toggle]');
+const pricing = {
+  pageviews: ['10k', '50k', '100k', '500k', '1m'],
+  prices: [8, 12, 16, 24, 36],
+  yearlyDiscount: 25,
+};
+
+slider.addEventListener('input', changePricingInfos);
+toggle.addEventListener('change', changeMonthlyPrice);
+
+changeSliderBackground();
+function changeSliderBackground() {
+  const value = (slider.value / (slider.max - slider.min)) * 100;
+
+  slider.style.backgroundImage = `linear-gradient(to right, var(--soft-cyan) 0%, var(--soft-cyan) ${value}%, var(--light-grayish-blue-100) ${value}%, var(--light-grayish-blue-100) 100%)`;
+}
+
+changePricingInfos();
+function changePricingInfos() {
+  changeSliderBackground();
+  changePageviewsValue();
+  changeMonthlyPrice();
+}
+
+function changeMonthlyPrice() {
+  const priceElement = document.querySelector('[data-price]');
+  const { prices } = pricing;
+  const sliderValue = getSliderValue();
+  const price = prices[sliderValue];
+  const discount = verifyDiscount();
+  const newPrice = price - (price * discount) / 100;
+
+  priceElement.innerText = `$${newPrice.toFixed(2)}`;
+}
+
+function getSliderValue() {
+  return slider.value;
+}
+
+function verifyDiscount() {
+  const { yearlyDiscount } = pricing;
+  const isChecked = toggle.checked;
+
+  if (isChecked) {
+    toggle.setAttribute('aria-labelledby', 'yearly-desc');
+    return yearlyDiscount;
+  }
+  toggle.setAttribute('aria-labelledby', 'monthly-desc');
+  return 0;
+}
+
+function changePageviewsValue() {
+  const pageviewsElement = document.querySelector('[data-pageviews]');
+  const { pageviews } = pricing;
+
+  pageviewsElement.innerText = pageviews[getSliderValue()];
+}
